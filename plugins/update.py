00ce5e73464683364b11710f09e636cdb1473b70
LikeJson <likejson@gmail.com>
2021-07-22 11:38:16 +0800

    del: useless code

--- plugins/update.py
+++ plugins/update.py
@@ -5,8 +5,6 @@ class pluginClass:
 
     async def onCommandMessageReceivedListener(client, event, command, args):
         import os
-        import sys
         os.system("git pull")
         await client.edit_message(event.message, "更新完成，正在重启 BotMaid")
-        # sys.exit(0)
         await client.disconnect()
